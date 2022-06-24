fb125b972a1003b4e0982de51c4910a0bd3207ee
KG LEE <acrkdduf@naver.com>
2022-06-25 02:14:44 +0900

    feat: react-router-dom 기본 구조 적용

--- client/src/web/App/App.tsx
+++ client/src/web/App/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Income from '../pages/Income';
+import Outcome from '../pages/Outcome';
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/outcome' element={<Outcome />} />
+        <Route path='/income' element={<Income />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
