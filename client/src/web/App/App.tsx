7fe21685bba6a40b824b80e58f69f98fe7361852
KG LEE <acrkdduf@naver.com>
2022-06-26 01:12:26 +0900

    feat: App 기본 스타일 적용

--- client/src/web/App/App.tsx
+++ client/src/web/App/App.tsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import routes from '../../routes';
+import * as Styled from './style';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {routes.list.map((route) => (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={route.pcComponent}
-          />
-        ))}
-      </Routes>
-    </BrowserRouter>
+    <Styled.App>
+      <BrowserRouter>
+        <Routes>
+          {routes.list.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={route.pcComponent}
+            />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </Styled.App>
   );
 }
 
