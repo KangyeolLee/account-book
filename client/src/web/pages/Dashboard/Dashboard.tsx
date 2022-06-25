b39e104076aeaaacaffbfc149d97ef8e0dfd0804
KG LEE <acrkdduf@naver.com>
2022-06-26 01:58:20 +0900

    feat: Dashboard 페이지 작성

--- client/src/web/pages/Dashboard/Dashboard.tsx
+++ client/src/web/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,15 @@
+import Menus from '../../domains/Dashboard/components/Menus';
+import Profile from '../../domains/Dashboard/components/Profile';
+
+import * as Styled from './style';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Styled.Dashboard>
+      <Profile />
+      <Menus />
+    </Styled.Dashboard>
+  );
 };
 
 export default Dashboard;
---
+++ client/src/web/pages/Dashboard/style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Dashboard = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
