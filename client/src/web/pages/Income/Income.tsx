d62ce14e6f4ac7566d918d913023fa1cc3cda47f
KG LEE <acrkdduf@naver.com>
2022-06-27 00:52:54 +0900

    feat: 수입 페이지 기본 레이아웃 작성

--- client/src/web/pages/Income/Income.tsx
+++ client/src/web/pages/Income/Income.tsx
@@ -1,5 +1,14 @@
+import History from '../../../common/components/History';
+import TotalAmount from '../../../common/components/TotalAmount';
+import * as Styled from './style';
+
 const Income = () => {
-  return <div>Income</div>;
+  return (
+    <Styled.Income>
+      <TotalAmount type='INCOME' />
+      <History />
+    </Styled.Income>
+  );
 };
 
 export default Income;
---
+++ client/src/web/pages/Income/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Income = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
