d9dbe9c941e84c3e88c49a73839d350ad0d65851
KG LEE <acrkdduf@naver.com>
2022-07-02 23:57:49 +0900

    fix: TotalAmount 컴포넌트 레이아웃 스타일 변경

--- client/src/common/components/TotalAmount/style.ts
+++ client/src/common/components/TotalAmount/style.ts
@@ -1,15 +1,19 @@
 import styled from 'styled-components';
 import COLOR from '../../../styles/colors';
+import { initBoxModel } from '../../../styles/helper';
 import Price from '../Price';
 
 export const TotalAmountWrapper = styled.article`
   border: 1px solid ${COLOR.GREY_500};
   border-radius: 4px;
   padding: 15px;
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const AmountTypeTitle = styled.h3`
   margin-top: 0;
+  ${initBoxModel};
 `;
 
 export const TotalAmount = styled(Price)``;
