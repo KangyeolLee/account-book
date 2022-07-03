fb0bd374cbc7acb1000487834488f4ebd128ed80
KG LEE <acrkdduf@naver.com>
2022-07-04 01:26:43 +0900

    feat: jotai 디버깅 컴포넌트 적용

--- client/src/entries/web.tsx
+++ client/src/entries/web.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import DebugAtoms from '../jotai/debugs/DebugAtoms';
 import { GlobalWebStyle } from '../styles/global';
 import App from '../web/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <DebugAtoms />
     <GlobalWebStyle />
     <App />
   </React.StrictMode>
