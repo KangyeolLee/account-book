58f712bda7cc5806f1d8005200b5072e31148d32
KG LEE <acrkdduf@naver.com>
2022-07-07 01:51:46 +0900

    chore: nest cli - Email services 계층 생성

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api-controller/api.controller';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { EmailService } from './email/email.service';
 
 @Module({
   imports: [],
   controllers: [ApiController, UsersController],
-  providers: [UsersService],
+  providers: [UsersService, EmailService],
 })
 export class AppModule {}
