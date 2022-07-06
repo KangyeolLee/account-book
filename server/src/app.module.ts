73be7d5c47c2add624ea6854010666d63a94660c
KG LEE <acrkdduf@naver.com>
2022-07-07 01:43:12 +0900

    chore: nest cli - user.service 계층 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api-controller/api.controller';
 import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [],
   controllers: [ApiController, UsersController],
-  providers: [],
+  providers: [UsersService],
 })
 export class AppModule {}
