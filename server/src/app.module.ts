080a8faa9b5c21daa8d5d3a33c82ec51b6756dc3
KG LEE <acrkdduf@naver.com>
2022-07-09 23:35:37 +0900

    feat: 이메일 관련 환경변수 모듈 적용

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
 
 import { UsersModule } from './users/users.module';
+import validationEmailConfig from './utils/validatationEmailConfig';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema: validationEmailConfig,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
