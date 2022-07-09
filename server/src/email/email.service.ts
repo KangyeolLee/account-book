f022d838ef18b61e75a009f56673be646e7e9b76
KG LEE <acrkdduf@naver.com>
2022-07-09 23:35:54 +0900

    feat: 이메일 환경변수 적용한 이메일 서비스 계층 작성

--- server/src/email/email.service.ts
+++ server/src/email/email.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import Mail = require('nodemailer/lib/mailer');
+import { Inject, Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import emailConfig from 'src/config/emailConfig';
+import { ConfigType } from '@nestjs/config';
+
+interface EmailOptions {
+  from: string;
+  to: string;
+  subject: string;
+  html: string;
+}
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  private transporter: Mail;
+
+  constructor(
+    @Inject(emailConfig.KEY) private config: ConfigType<typeof emailConfig>,
+  ) {
+    this.transporter = nodemailer.createTransport({
+      service: config.service,
+      host: config.smtp_host,
+      port: config.port,
+      auth: {
+        user: config.auth.user,
+        pass: config.auth.pass,
+      },
+    });
+  }
+
+  async sendMemberJoinVerification(
+    emailAddress: string,
+    signupVerifyToken: string,
+  ) {
+    const baseUrl = this.config.baseUrl;
+    const url = `${baseUrl}/users/email-verify?signupVerifyToken=${signupVerifyToken}`;
+
+    const mailOptions: EmailOptions = {
+      from: 'acrkdduf@naver.com',
+      to: emailAddress,
+      subject: '가입 인증 메일',
+      html: `
+        가입확인 버튼을 누르시면 가입 인증이 완료됩니다. <br />
+        <form action="${url}" method="POST">
+          <button>가입확인</button>
+        </form>
+      `,
+    };
+
+    return await this.transporter.sendMail(mailOptions);
+  }
+}
