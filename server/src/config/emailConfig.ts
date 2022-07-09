8b998f5a731eced6af332d9cb312138b2855f0a4
KG LEE <acrkdduf@naver.com>
2022-07-09 23:34:41 +0900

    feat: EmailConfig 모듈 작성

--- server/src/config/emailConfig.ts
+++ server/src/config/emailConfig.ts
@@ -1,10 +1,14 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('email', () => ({
-  service: process.env.EMAIL_SERVICE,
-  auth: {
-    user: process.env.EMAIL_AUTH_USER,
-    pass: process.env.EMAIL_AUTH_PASSWORD,
-  },
-  baseUrl: process.env.EMAIL_BASE_URL,
-}));
+export default registerAs('email', () => {
+  return {
+    service: process.env.EMAIL_SERVICE,
+    smtp_host: process.env.EMAIL_SMTP_HOST,
+    port: +process.env.EMAIL_SMTP_HOST_PORT,
+    auth: {
+      user: process.env.EMAIL_AUTH_USER,
+      pass: process.env.EMAIL_AUTH_PASSWORD,
+    },
+    baseUrl: process.env.EMAIL_BASE_URL,
+  };
+});
