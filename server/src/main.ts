d451c0e284abfb838305778c56a649dc689307ab
KG LEE <acrkdduf@naver.com>
2022-07-15 01:39:09 +0900

    feat: app.use()를 통해 글로벌 미들웨어 등록

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { globalLogger } from './middlewares/Logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.use(globalLogger);
   await app.listen(4000);
 }
 bootstrap();
